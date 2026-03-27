75e80cbc2f8dc400dfb9acd3207de4bcec359e77
sanjanab1 <183420321+sanjanab1@users.noreply.github.com>
2026-03-27 02:02:13 -0700

    wrapping app in error boundaries

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ListingsPage from './pages/ListingsPage';
 import PropertyDetailPage from './pages/PropertyDetailPage';
 import './App.css';
+import { useNavigate } from 'react-router-dom';
+import ErrorBoundary from './components/ErrorBoundary';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<ListingsPage />} />
-          <Route path="/property/:id" element={<PropertyDetailPage />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<ListingsPage />} />
+            <Route path="/property/:id" element={<PropertyDetailPage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
+
 export default App;
